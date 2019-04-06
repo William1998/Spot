580e390e8d2e3b13c1fe28f958952899d9597564
Ziyi Guo <zguo0013@student.monash.edu>
2019-04-06 14:13:00 +1100

    second commit with swipe tabs

--- app/src/main/java/edu/monash/hackamon/spot/MainActivity.java
+++ app/src/main/java/edu/monash/hackamon/spot/MainActivity.java
@@ -1,11 +1,17 @@
 package edu.monash.hackamon.spot;
 
 import android.content.Intent;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.TabLayout;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabAdapter adapter;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     TextView yourLocation;
 
@@ -15,6 +21,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+
+        adapter = new TabAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Tab1Fragment(), "Tab 1");
+        adapter.addFragment(new Tab2Fragment(), "Tab 2");
+        adapter.addFragment(new Tab3Fragment(), "Tab 3");
+        adapter.addFragment(new Tab4Fragment(), "Tab 4");
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
     public void getYourLocation() {}
